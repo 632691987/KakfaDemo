286b4c915caf6f628be6f8975d0c5d9aed058568
VincentZhang <vincentzhang@outlook.es>
2021-07-04 03:04:53 +0200

    Enhance

--- src/main/java/com/example/kakfa/demo/controller/ProducerController.java
+++ src/main/java/com/example/kakfa/demo/controller/ProducerController.java
@@ -3,7 +3,7 @@ package com.example.kakfa.demo.controller;
 import java.util.UUID;
 import javax.annotation.Resource;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +12,7 @@ public class ProducerController {
   @Resource
   private KafkaTemplate<String, Object> kafkaTemplate;
 
-  @RequestMapping("/send")
+  @GetMapping("/send")
   public void send() {
     kafkaTemplate.send("topic1", "Message body: [" + UUID.randomUUID() + "]");
   }
